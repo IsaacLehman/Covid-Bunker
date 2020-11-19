abcc7b09c4489c1ed6cb7d1d22d37d497e98439a
EricMartin17 <44842271+EricMartin17@users.noreply.github.com>
2020-11-19 17:10:58 -0500

    Fixed admin image

--- covid-bunker/static/js/Admin-Product.js
+++ covid-bunker/static/js/Admin-Product.js
@@ -1,6 +1,20 @@
-window.addEventListener("DOMContentLoaded", function() {
-   // let categoryElement = document.getElementById("category");
-    //let categoryValue = categoryElement.value;
-    //console.log(categoryValue);
 
-});
+
+var openFile = function(event) {
+    var input = event.target;
+
+    // Instantiate FileReader
+    var reader = new FileReader();
+    reader.onload = function(){
+        TheFileContents = reader.result;
+        // Update the output to include the <img> tag with the data URL as the source
+
+        image = document.querySelector(".admin-product-picture"); 
+        image.src = TheFileContents;
+        image.style.visibility = "visible";
+
+    };
+    // Produce a data URL (base64 encoded string of the data in the file)
+    // We are retrieving the first file from the FileList object
+    reader.readAsDataURL(input.files[0]);
+};
