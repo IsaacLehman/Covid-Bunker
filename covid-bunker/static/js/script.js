e864996954291062c326ab7a7bc1a8225b441fda
IsaacLehman <iikkeelloo@gmail.com>
2020-11-19 16:28:18 -0500

    Scroll to top btn added.

--- covid-bunker/static/js/script.js
+++ covid-bunker/static/js/script.js
@@ -3,5 +3,25 @@
 // add a listener so that when the document loads . . .
 // new listeners  can be attched to elements safely
 window.addEventListener("DOMContentLoaded", function() {
+  /* SCROLL TO TOP */
+  //Get the button
+  var topButton = document.getElementById("topBtn");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      topButton.style.display = "block";
+    } else {
+      topButton.style.display = "none";
+    }
+  }
 
 });
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
